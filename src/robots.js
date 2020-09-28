57f7576622ff0b0cfa37b6dbb41216c44f2bcfd2
Alvin Joy <68543356+alvinjoy93@users.noreply.github.com>
2020-09-28 15:59:57 +0530

    Add files via upload

---
+++ src/robots.js
@@ -0,0 +1,63 @@
+export const robots =[
+{
+	id:0,
+	name:'Rasbin Raieessee',
+	username:'Mnky',
+	email:'rasbi@gmail.com'
+},
+{
+	id:1,
+	name:'Joshna Joy',
+	username:'Josh',
+	email:'joshna@gmail.com'
+},
+{
+	id:2,
+	name:'Joel Antony',
+	username:'Jolly',
+	email:'jolly@gmail.com'
+},
+
+{
+	id:3,
+	name:'Neel Sebastian',
+	username:'Neelutty',
+	email:'neel@gmail.com'
+},
+{
+	id:4,
+	name:'Sonal KS',
+	username:'Soddi',
+	email:'soddi@gmail.com'
+},
+{
+	id:5,
+	name:'Naima A',
+	username:'Naimu',
+	email:'Naimu@gmail.com'
+},
+{
+	id:6,
+	name:'Salvin Joseph',
+	username:'Salvi',
+	email:'Salvi@gmail.com'
+},
+{
+	id:7,
+	name:'Nikhil Jose',
+	username:'Nikhilu',
+	email:'Nikhilu@gmail.com'
+},
+{
+	id:8,
+	name:'Devika Reji',
+	username:'Devu',
+	email:'Devu@gmail.com'
+},
+{
+	id:9,
+	name:'Amrita Dileep',
+	username:'Ammu',
+	email:'Ammu@gmail.com'
+}
+]
